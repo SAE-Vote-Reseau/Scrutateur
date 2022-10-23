175a79f5ceeb5cf4b21e9f6e0a71a689698fe596
Lengthrequired <hyp3rcr4ft@gmail.com>
2022-10-23 18:42:06 +0200

    refactor pour permettre de se connecter avec le Serveur et possibilité de recup les résultats (il faut faire en sorte de pouvoir recup les vrais resultats de vote car pour l'instant ils sont "fake" )

--- src/main/java/vote/Scrutateur/Scrutateur.java
+++ src/main/java/vote/Scrutateur/Scrutateur.java
@@ -2,13 +2,14 @@ package vote.Scrutateur;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.out;
 
-public class main{
+public class Scrutateur {
     public static void main(String[] args) {
 
 
@@ -37,20 +38,24 @@ public class main{
     }
 
     public static void GetResults() throws IOException, ClassNotFoundException {
-            Socket socket = new Socket("127.0.0.1", 25565);
-            java.io.DataOutputStream out = new java.io.DataOutputStream(socket.getOutputStream());
+            Socket socket = new Socket("127.0.0.1", 5565);
+            java.io.ObjectOutputStream out = new java.io.ObjectOutputStream(socket.getOutputStream());
             out.writeUTF("getResults");
+            out.flush();
             System.out.println("Demande de résultats envoyée");
+
             java.io.ObjectInputStream in = new java.io.ObjectInputStream(socket.getInputStream());
+
             String[] Results = (String[]) in.readObject();
+            System.out.println("Résultats reçus");
             PrintResults(Results);
             in.close();
             out.close();
     }
 
     public static void PrintResults(String[] Results){
-        System.out.println("Résultats du vote : "+ "\"" +Results[4]+"\"");
-        System.out.println( Results[2]+" : " + Results[0]);
-        System.out.println(Results[3]+" : " + Results[1]);
+        System.out.println("Résultats du vote : "+ "\"" +Results[0]+"\"");
+        System.out.println( Results[1]+" : " + Results[3]);
+        System.out.println(Results[2]+" : " + Results[4]);
     }
 }
